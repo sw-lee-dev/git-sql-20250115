0bce4db6a773d2b2b5ce6d04c8500237f9891e75
sw-lee-dev <lsw6212@naver.com>
2025-02-06 11:06:47 +0900

    2025.02.06 11:00

---
+++ SQL_data_link.sql
@@ -0,0 +1,12 @@
+USE data;
+
+CREATE TABLE airport2 (
+	e_airport VARCHAR(100),
+    k_airport VARCHAR(100),
+    iata_airport VARCHAR(5),
+    icao_airport VARCHAR(5),
+    location VARCHAR(30),
+    e_country VARCHAR(100),
+    k_country VARCHAR(100),
+    e_city VARCHAR(100)
+);
